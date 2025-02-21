d46b4fbf208b833541a2458e6cc60ca35d48c261
OFFTijn <tijnm06@gmail.com>
2025-02-21 16:23:53 +0100

    update footer link to point to new GitHub profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/OFFTijn'
       className='link footer__link'
     >
       &copy; 2025 - Tijn Mestrom
